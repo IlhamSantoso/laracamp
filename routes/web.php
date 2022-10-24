d1c0a495e586a4906e230b25fa9f8b47b10c320c
ilhamsanns <i.santoso024@gmail.com>
2022-10-24 23:03:20 +0700

    install & integrated sociallate

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,6 +26,9 @@ Route::get('success-checkout', function () {
     return view('success_checkout');
 })->name('success-checkout');
 
+//sociallite routes
+Route::get('sign-in-goole', [UserController::class, 'google'])->name('user-login-google');
+Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user-google-callback');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
